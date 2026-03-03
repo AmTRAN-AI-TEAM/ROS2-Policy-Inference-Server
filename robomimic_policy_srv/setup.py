e52e7fc8b501049ef6e1d93a35c7f88511797d80
longann <longann890621@gmail.com>
2026-03-03 15:30:27 +0800

    Remove model installation from setup.py

--- robomimic_policy_srv/setup.py
+++ robomimic_policy_srv/setup.py
@@ -10,7 +10,6 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/models', ['models/model_epoch_20.pth']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
